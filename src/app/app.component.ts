2a4d317698b777b70e0e3ce04797cde2b2f69618
Zsolt Schutzbach <succ92@gmail.com>
2018-08-13 18:59:17 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fmhun-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fm19-hun-db';
+}
